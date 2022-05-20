fb796c910bb6a96dd8685dd3dbbb21144d247dae
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-05-21 00:38:12 +0200

    defined the roles

---
+++ src/RSA_Muns.cpp
@@ -0,0 +1,6 @@
+#include "RSA.h"
+
+bool RSA::millerRabinIsPrime(uint num) const {}
+int RSA::extendedEuclidean(int a, int b, int &x, int &y) const {}
+void RSA::calculateInternals() {}
+string RSA::decrypt(const vector<uint> &cipher) const {}
---
+++ src/RSA_Yasser.cpp
@@ -0,0 +1,8 @@
+#include "RSA.h"
+
+uint RSA::fastExponentiation(uint base, uint power) const {}
+void RSA::calculateExternals() {}
+RSA::RSA() {}
+pair<uint, uint> RSA::getPublicKey() const {}
+vector<uint> RSA::encrypt(const string &plain, const uint &e,
+                          const uint &n) const {}
